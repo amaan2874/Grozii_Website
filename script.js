20de98b2fed058931d9014315720379b557fb03a
farhan2874 <amaanraza4905@gmail.com>
2024-12-29 14:45:00 +0530

    all perfect

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const logoImage = document.querySelector(".logo-image");
+    logoImage.src = "images/Coming Soon.png"; // Replace with your image URL
+  });
+  
