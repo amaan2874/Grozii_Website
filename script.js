058583c283e84fd5a9838602a8e53e5e22414a35
farhan2874 <amaanraza4905@gmail.com>
2025-01-02 16:25:32 +0530

    some changes

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     logoImage.src = "images/Coming Soon.png"; // Replace with your image URL
   });
   
+
+  // Select hamburger menu and nav links
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+// Toggle visibility on click
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
